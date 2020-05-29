1139aa17a86ac0498c7cac61796a8e47909d10f9
MaFernanda <1718110411@utectulancingo.edu.mx>
2020-05-29 03:07:22 +0000

    Fecha y hora terminado.

--- semana_3/mvc/controllers/visitas.py
+++ semana_3/mvc/controllers/visitas.py
@@ -1,10 +1,13 @@
 import web
+import datetime
 
 class Visitas:
   def GET(self, nombre):
     try:
       cookie = web.cookies()
       visitas = "0"
+      dt = datetime.datetime.now()
+      fecha = dt.strftime("%c")
       print(cookie)
       if nombre:
         web.setcookie("nombre",nombre,expires="", domain=None)
@@ -16,11 +19,10 @@ class Visitas:
         visitas = int(cookie.get("visitas"))
         visitas += 1
         web.setcookie("visitas", str(visitas), expires="", domain=None)
-        return "Visitas " + str(visitas)
       else:
         web.setcookie("visitas", str(1), expires="", domain=None)
         visitas = "1" 
 
-      return "Visitas " + str(visitas) + " Nombre " + nombre
+      return "Visitas " + str(visitas) + "   Nombre " + nombre + "   Fecha y hora: " + str(fecha)
     except Exception as e:
       return "Error " + str(e.args)
